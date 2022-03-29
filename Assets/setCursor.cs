0eeda2234145aef8211a8e56bbb1364e65f96f0d
Funboringness <ldroberts02@gmail.com>
2022-03-29 10:39:48 -0500

    Animated Cursor test

--- Assets/setCursor.cs
+++ Assets/setCursor.cs
@@ -8,16 +8,34 @@ using UnityEngine;
 public class setCursor : MonoBehaviour
 {
     // You must set the cursor in the inspector.
-    public Texture2D crosshair;
+    public List<Texture2D> crosshair;
+    public int index = 0;
 
     void Start()
     {
 
         //set the cursor origin to its centre. (default is upper left corner)
-        Vector2 cursorOffset = new Vector2(crosshair.width / 2, crosshair.height / 2);
+        Vector2 cursorOffset = new Vector2(crosshair[0].width / 2, crosshair[0].height / 2);
 
         //Sets the cursor to the Crosshair sprite with given offset 
         //and automatic switching to hardware default if necessary
-        Cursor.SetCursor(crosshair, cursorOffset, CursorMode.Auto);
+        Cursor.SetCursor(crosshair[0], cursorOffset, CursorMode.Auto);  
     }
+ 
+    public void updateCursor()
+    {
+        index++;
+        if (index >= crosshair.Count)
+            index = 0;
+
+        //set the new cursor
+        Vector2 cursorOffset = new Vector2(crosshair[index].width / 2, crosshair[index].height / 2);
+
+        //Sets the cursor to the Crosshair sprite with given offset 
+        //and automatic switching to hardware default if necessary
+        Cursor.SetCursor(crosshair[index], cursorOffset, CursorMode.Auto);
+    }
+
+
+
 }

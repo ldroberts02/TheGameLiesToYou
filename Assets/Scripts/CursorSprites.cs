0b3fc1ff81bd02a25da53a29f2a6adddd1f41101
Funboringness <ldroberts02@gmail.com>
2022-04-12 10:52:10 -0500

    Reworked Cursor System
    changed cursor system to work with listed cursors, to allow for changing the cursor with public variables

---
+++ Assets/Scripts/CursorSprites.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class CursorSprites
+{
+    public string CursorName;
+    public List<Texture2D> Sprites;
+}
--- Assets/Scripts/setCursor.cs
+++ Assets/Scripts/setCursor.cs
@@ -2,37 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class setCursor : MonoBehaviour
 {
-    // You must set the cursor in the inspector.
-    public List<Texture2D> crosshair;
+
+    public Timer timer;
+    public List<CursorSprites> cursorSprites;
     public int index = 0;
+    public string cursorName;
+
 
     void Start()
     {
+        timer.AutoRestart = true;
+        timer.StartTimer(0.16f,true);
+        // SetCursor
+        UpdateCursor();
 
-        //set the cursor origin to its centre. (default is upper left corner)
-        Vector2 cursorOffset = new Vector2(crosshair[0].width / 6, crosshair[0].height / 6);
 
-        //Sets the cursor to the Crosshair sprite with given offset 
-        //and automatic switching to hardware default if necessary
-        Cursor.SetCursor(crosshair[0], cursorOffset, CursorMode.Auto);  
     }
- 
-    public void updateCursor()
+
+    public void UpdateCursor()
     {
+        //cs is local variable for update cursor using cursorsprites
+        CursorSprites cs = new CursorSprites();
+
+        for (int i = 0; i < cursorSprites.Count; i++)
+            if (cursorSprites[i].CursorName == cursorName)
+                cs = cursorSprites[i];
+
         index++;
-        if (index >= crosshair.Count)
+
+        if (index >= cs.Sprites.Count)
             index = 0;
-
-        //set the new cursor
-        Vector2 cursorOffset = new Vector2(crosshair[index].width / 6, crosshair[index].height / 6);
-
-        //Sets the cursor to the Crosshair sprite with given offset 
-        //and automatic switching to hardware default if necessary
-        Cursor.SetCursor(crosshair[index], cursorOffset, CursorMode.Auto);
+        Cursor.SetCursor(cs.Sprites[index], Vector2.zero, CursorMode.Auto);
     }
-
-
 
 }
